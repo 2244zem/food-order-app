c1763e9a1938c8275cb0428551fccd64604c5113
Muhammad Iqra Zemylev <fiksfaa@gmail.com>
2024-11-11 12:45:46 +0700

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 document.getElementById("orderButton").onclick = function() {
+
     let foodprice = parseInt(document.getElementById("food").value);
     let drinkprice = parseInt(document.getElementById("drink").value);
-
+    
     let totalprice = foodprice + drinkprice;
-    alert(totalprice);
-
     let discount = 0;
-
-    if(totalprice > 90) {
-        alert('ok')
+    
+    if(totalprice > 100) {
+        discount = totalprice * 0.1;
+        totalprice -= discount;
     }
-
+    
+    const resultDiv = document.getElementById("result");
+    if (totalprice > 0) {
+        resultDiv.innerHTML = `
+        Total : $${foodprice + drinkprice}<br>
+        Discount : $${discount.toFixed(2)}<br>
+        Final Price : $${totalprice.toFixed(2)}
+        `;
+    } else{
+        resultDiv.innerHTML = "Please select both food and drink"
+    }
+    
+    
 }
